71022fa033a65f1b8d9b8305dcf0151826afbe7c
Charles <charles@charlesen.fr>
2024-02-20 10:47:00 +0100

    Reinit pwd

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,6 +25,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank]
     private ?string $email = null;
 
     #[ORM\Column]
