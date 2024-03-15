2a4d98e2c04bdf9f5cafbb7ba5bfa6e79b4fb206
Charles <charles@charlesen.fr>
2024-03-15 16:57:12 +0100

    Page 404 si nécessaire

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Page;
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/{slug}', name: 'page_show')]
-    public function show(Page $page): Response
+    public function show(string $slug, PageRepository $pageRepository): Response
     {
+        $page = $pageRepository->findOneBy(['slug' => $slug]);
+        if (!$page) {
+            throw $this->createNotFoundException();
+        }
         return $this->render('page/show.html.twig', [
             'page' => $page,
         ]);
