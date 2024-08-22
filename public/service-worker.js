2def610f5c16d804bfeb6ddea8bc3f38697184a6
Charles <charles@charlesen.fr>
2024-08-22 10:24:36 +0200

    Maj service worker online

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,12 @@
 // Nom du cache
-const CACHE_VERSION = "0.0.1";
+const CACHE_VERSION = "0.0.2";
 const CACHE_NAME = "pepiteclub-cache-v" + CACHE_VERSION;
+const MAX_CACHE_AGE = 1 * 60 * 60 * 1000; // 1 heure en millisecondes
 
 // Liste des fichiers à mettre en cache
 const URLS_TO_CACHE = [
   "/",
+  "/courses",
   "/assets/styles/",
   "/assets/styles/*.css",
   "/assets/images/*.jpg",
@@ -25,12 +27,6 @@ const URLS_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   console.log("SW::install :: event", event);
-
-  // event.waitUntil(
-  //   caches.open(CACHE_NAME).then((cache) => {
-  //     return cache.addAll(URLS_TO_CACHE);
-  //   })
-  // );
 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -72,36 +68,58 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
       const cachedResponse = await caches.match(event.request);
+
+      // Si une réponse en cache existe
       if (cachedResponse) {
+        const cachedDate = new Date(cachedResponse.headers.get("date"));
+        const now = new Date();
+        const isConnected = navigator.onLine; // Vérifie si l'utilisateur est en ligne
+
+        // Si l'utilisateur est en ligne et le cache est plus vieux que la durée de cache max, on le rafraîchit
+        if (isConnected && now - cachedDate > MAX_CACHE_AGE) {
+          console.log(
+            `[Service Worker] Cache too old, refreshing: ${event.request.url}`
+          );
+          return fetchAndUpdateCache(event.request, cachedResponse);
+        }
+
         console.log(
           `[Service Worker] Serving cached resource: ${event.request.url}`
         );
         return cachedResponse;
       }
-      try {
-        const response = await fetch(event.request);
-        if (response.status === 404) {
-          console.error(
-            `[Service Worker] Resource not found: ${event.request.url}`
-          );
-          return new Response("Resource not found", { status: 404 });
-        }
-        const cache = await caches.open(CACHE_NAME);
-        console.log(
-          `[Service Worker] Caching new resource: ${event.request.url}`
-        );
-        cache.put(event.request, response.clone());
-        return response;
-      } catch (error) {
-        console.error(
-          `[Service Worker] Fetch failed; returning offline page instead: ${event.request.url}`,
-          error
-        );
-        return new Response(
-          "Service non disponible. Merci de recommencer dans quelques instants ou vérifier la connexion internet",
-          { status: 503 }
-        );
-      }
+
+      // Si aucune réponse en cache, on va chercher la ressource en ligne
+      return fetchAndUpdateCache(event.request);
     })()
   );
 });
+
+// Fonction pour récupérer une ressource en ligne et mettre à jour le cache
+async function fetchAndUpdateCache(request, fallbackResponse) {
+  try {
+    const response = await fetch(request);
+
+    if (response.status === 404) {
+      console.error(`[Service Worker] Page non trouvée: ${request.url}`);
+      return (
+        fallbackResponse || new Response("Page non trouvée", { status: 404 })
+      );
+    }
+
+    const cache = await caches.open(CACHE_NAME);
+    console.log(`[Service Worker] Caching new resource: ${request.url}`);
+    cache.put(request, response.clone());
+
+    return response;
+  } catch (error) {
+    console.error(`[Service Worker] Fetch failed: ${request.url}`, error);
+    return (
+      fallbackResponse ||
+      new Response(
+        "Service non disponible. Merci de recommencer dans quelques instants ou vérifier la connexion internet",
+        { status: 503 }
+      )
+    );
+  }
+}
